d11e551e63e1c095e6f7d72e00683e4b0aa292df
Tim Hacker <tim.hacker@madetech.com>
2020-10-28 15:13:26 +0000

    Added blank id test and changed to store submitted object

--- handler.js
+++ handler.js
@@ -2,13 +2,21 @@ const { saveRecord } = require("./dependencies");
 const { nanoid } = require("nanoid");
 
 module.exports.data = async (event) => {
-  if (!event || !event.body || !event.body.id) {
+  if (!event || !event.body) {
     return { statusCode: 400 };
   }
-  const record = { id: nanoid(6) };
+  const request = JSON.parse(event.body);
+
+  if (!request.id) { // This check needs to change -- either 'name' or just any object
+    return { statusCode: 400 };
+  }
+
+  //const record = { id: nanoid(6) };
+
+  let result;
 
   try {
-    const result = await saveRecord(record);
+    result = await saveRecord(request);
   } catch (error) {
     return {
       statusCode: 500,
@@ -18,6 +26,6 @@ module.exports.data = async (event) => {
 
   return {
     statusCode: 200,
-    body: JSON.stringify(record),
+    body: JSON.stringify(result), //This should be result
   };
 };
--- handler.test.js
+++ handler.test.js
@@ -4,6 +4,11 @@ const { saveRecord } = require("./dependencies");
 const { data } = require("./handler");
 
 describe("handler", () => {
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  })
+
   it("returns a 400 if event does not contain a body", async () => {
     const result = await data();
 
@@ -11,7 +16,15 @@ describe("handler", () => {
   });
 
   it("returns a 400 if event body does not contain an id", async () => {
-    const event = { body: { invalid: "object" } };
+    const event = { body: JSON.stringify({ invalid: "object" }) };
+
+    const result = await data(event);
+
+    expect(result.statusCode).toBe(400);
+  });
+
+  it("returns a 400 if event body contains a blank id", async () => {
+    const event = { body: JSON.stringify({ id: "" }) };
 
     const result = await data(event);
 
@@ -19,15 +32,27 @@ describe("handler", () => {
   });
 
   it("returns a 500 with error if saveRecord throws an exception", async () => {
-    const event = { body: { id: "1234" } };
+    const event = { body: JSON.stringify({ id: "1234" }) };
 
     saveRecord.mockImplementationOnce(() => { throw "error saving" })
 
     const result = await data(event);
 
     expect(result.statusCode).toBe(500);
-    expect(result.body).toEqual(JSON.stringify({"error":"error saving"}));
+    expect(result.body).toEqual(JSON.stringify({"error": "error saving"}));
   })
 
-  // Happy path where it saves :)
+  it("saves a record when valid input is provided", async () => {
+    const event = { body: JSON.stringify({ id: "1234" }) };
+    
+    const result = await data(event);
+
+    expect(saveRecord.mock.calls.length).toBe(1);
+    expect(saveRecord.mock.calls[0][0]).toStrictEqual({ id: "1234" });
+    expect(result.statusCode).toBe(200);
+  })
+
+
+
+  // Batch insert with new endpoint
 });
