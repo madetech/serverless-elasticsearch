95d00910819ace09035d5f90712f41912d2329a8
Joe Roberts <joe.roberts@madetech.com>
2020-10-29 16:06:50 +0000

    explode = true

--- stream.js
+++ stream.js
@@ -17,12 +17,18 @@ module.exports.run = async (event) => {
     for(const record of event.Records) {
       const obj = Converter.unmarshall(record.dynamodb.NewImage)
       console.log(obj);
-
-      const result = await client.create({
-        id: obj.id,
-        index: 'bar',
-        body: obj
-      });
+      try {
+        if (obj.explode === true)
+          throw "This is an error."
+        
+        const result = await client.create({
+          id: obj.id,
+          index: 'bar',
+          body: obj
+        });
+      } catch (err) {
+        console.error(`Error on ${obj.id}: ${err.message}.`)
+      }
   
       console.log(`result: ${result}`);
     }
