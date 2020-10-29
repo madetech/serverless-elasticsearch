aca7ab5d783c7735e9ce8a1c1139c2e9a01c56a4
Joe Roberts <joe.roberts@madetech.com>
2020-10-29 15:57:32 +0000

    It now pushes into ES

--- stream.js
+++ stream.js
@@ -3,6 +3,7 @@ const {
   createAWSConnection,
   awsGetCredentials,
 } = require("@acuris/aws-es-connection");
+const { Converter } = require('aws-sdk/clients/dynamodb')
 
 module.exports.run = async (event) => {
   const awsCredentials = await awsGetCredentials();
@@ -13,17 +14,18 @@ module.exports.run = async (event) => {
   });
 
   try {
-    event.Records.forEach((record) => {
-      console.log(record.dynamodb.NewImage);
+    for(const record of event.Records) {
+      const obj = Converter.unmarshall(record.dynamodb.NewImage)
+      console.log(obj);
 
       const result = await client.create({
-        id: record.dynamodb.NewImage.id,
+        id: obj.id,
         index: 'bar',
-        body: record.dynamodb.NewImage
+        body: obj
       });
   
       console.log(`result: ${result}`);
-    });
+    }
     
   } catch (err) {
     console.error(JSON.stringify({ err }));
