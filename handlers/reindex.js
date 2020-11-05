aad3f6637204008f795e95ba5f2bb1b3a0ed2128
Matt Millar <matthew.millar@madetech.com>
2020-11-05 13:42:19 +0000

    Fix reindex

--- handlers/reindex.js
+++ handlers/reindex.js
@@ -16,32 +16,41 @@ module.exports.run = async (event) => {
   const dynClient = new DynamoDB();
 
   console.log(`Fetching records from ${process.env.DB_NAME}...`);
-  const result = await dynClient
+  const dynamoResult = await dynClient
     .scan({
       TableName: process.env.DB_NAME,
     })
     .promise();
 
-  console.log(`Reindexing records...`);
-  for (const [i, item] of result.Items.entries()) {
+  console.log(`Reindexing ${dynamoResult.Items.length} records...`);
+  for (const [i, item] of dynamoResult.Items.entries()) {
     const recordObj = DynamoDB.Converter.unmarshall(item);
 
     try {
       recordObj["@timestamp"] = new Date();
-      const result = await esClient.update({
+      const esResult = await esClient.update({
         id: recordObj.id,
         index: process.env.ES_INDEX_NAME,
-        body: recordObj,
+        body: {
+          doc: recordObj,
+          doc_as_upsert: true,
+        },
       });
       console.log(
-        `Indexed ${i + 1} of ${event.Records.length} records: ${JSON.stringify(
-          result.meta.request.params.body
-        )}`
+        `Indexed ${i + 1} of ${
+          dynamoResult.Items.length
+        } records: ${JSON.stringify(esResult.meta.request.params.body)}`
       );
     } catch (error) {
       console.error(error);
     }
   }
 
+  const indexSize = await esClient.count({
+    index: process.env.ES_INDEX_NAME,
+  });
+  console.log(
+    `Index ${process.env.ES_INDEX_NAME} now contains ${indexSize.body.count} items.`
+  );
   console.log("REINDEX COMPLETE");
 };
